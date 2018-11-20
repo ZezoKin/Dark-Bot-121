69d3815fc2441640a493af4f0c1a3f8d533820a3
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-20 22:32:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('message', message => {
-	 var prefix = "#";
-  if(message.content.startsWith(prefix + "اقتراح")) {
-  await  message.channel.send(`اكتب اقتراحك الان`)
-    let filter = m => m.author.id === message.author.id
-      var text = '';
-        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
-          .then(co => {
-            text = co.first().content
-
-              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
-                client.channels.get("495359919529263116").send(`${message.author.username}'s sug => ${text}`)
-
-              })
-            }
-          })
-});
+        if(message.content === prefix + "hc") {
+        if(!message.channel.guild) return;
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You Dont Have Perms ❌');
+               message.channel.overwritePermissions(message.guild.id, {
+               READ_MESSAGES: false
+   })
+                message.channel.send('Channel Hided Successfully ! ✅  ')
+   }
+  });
 
 client.login(process.env.BOT_TOKEN);
