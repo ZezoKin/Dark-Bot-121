60b4999742c08a3847608db3d6d5b69195b9380b
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-20 22:25:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('message', async message =>{
-  if (message.author.boss) return;
-	var prefix = "!";
-if (!message.content.startsWith(prefix)) return;
-	let command = message.content.split(" ")[0];
-	 command = command.slice(prefix.length);
-	let args = message.content.split(" ").slice(1);
-	if (command == "mute") {
-		if (!message.channel.guild) return;
-		if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("انت لا تملك صلاحيات !! ").then(msg => msg.delete(5000));
-		if(!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.reply("البوت لايملك صلاحيات ").then(msg => msg.delete(5000));;
-		let user = message.mentions.users.first();
-		let muteRole = message.guild.roles.find("name", "Muted");
-		if (!muteRole) return message.reply("** لا يوجد رتبة الميوت 'Muted' **").then(msg => {msg.delete(5000)});
-		if (message.mentions.users.size < 1) return message.reply('** يجب عليك المنشن اولاً **').then(msg => {msg.delete(5000)});
-		let reason = message.content.split(" ").slice(2).join(" ");
-		message.guild.member(user).addRole(muteRole);
-		const muteembed = new Discord.RichEmbed()
-		.setColor("RANDOM")
-		.setAuthor(`Muted!`, user.displayAvatarURL)
-		.setThumbnail(user.displayAvatarURL)
-		.addField("**:busts_in_silhouette:  المستخدم**",  '**[ ' + `${user.tag}` + ' ]**',true)
-		.addField("**:hammer:  تم بواسطة **", '**[ ' + `${message.author.tag}` + ' ]**',true)
-		.addField("**:book:  السبب**", '**[ ' + `${reason}` + ' ]**',true)
-		.addField("User", user, true)
-		message.channel.send({embed : muteembed});
-		var muteembeddm = new Discord.RichEmbed()
-		.setAuthor(`Muted!`, user.displayAvatarURL)
-		.setDescription(`      
-${user} انت معاقب بميوت كتابي بسبب مخالفة القوانين
-${message.author.tag} تمت معاقبتك بواسطة
-[ ${reason} ] : السبب
-اذا كانت العقوبة عن طريق الخطأ تكلم مع المسؤلين
-`)
-		.setFooter(`في سيرفر : ${message.guild.name}`)
-		.setColor("RANDOM")
-	user.send( muteembeddm);
-  }
-});
+client.on('message', message => {
+	 var prefix = "#";
+  let args = message.content.split(" ").slice(1);
+if (message.content.startsWith(prefix + "kick")) {
+ if (message.channel.type == "dm") return;
+ if (message.author.bot) return;
+ if (message.author.codes) return;
+ if (message.author.kick) return;
+               
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return;
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return;
+  let user = message.mentions.users.first();
+ 
 
+  if (message.mentions.users.size < 1) return message.reply("**Mention a User to Kick Him.**").then(message => message.delete(5000))
+ 
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**I Can’t Kick This User**").then(message => message.delete(5000))
+
+  message.guild.member(user).kick(7, user);
+
+  message.channel.send(`**${user.tag} Kicked From the Server.**`).then(message => message.delete(10000))
+
+}
+});
 
 client.login(process.env.BOT_TOKEN);
