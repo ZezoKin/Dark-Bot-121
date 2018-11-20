e9df80de26c6b838641f27dfb6a293534f52a260
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-20 20:31:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('message', message => {
-          var prefix = "#";
          if (message.content.startsWith(prefix + "inv")) {
          if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
                  if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
