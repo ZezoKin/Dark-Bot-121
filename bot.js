8508e9658e72b2e63679b931d86473ba1f5563a4
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-20 20:21:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('message', message => {
-           var prefix = "#";
-client.on('message', message => {
-        if (message.content === prefix + "inv") {
-            if(!message.channel.guild) return;
-        let embed = new Discord.RichEmbed()
-        .setTitle(`:small_orange_diamond: Click Here To Invite Fantastic. `)
-        .setURL(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
-     message.channel.sendEmbed(embed);
-       }
-   });
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply(" كيف اقدر اساعدك!!");
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
