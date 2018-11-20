80e4e081d5f33e7fd4cf4c4ffce312e4303dfd5c
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-20 20:24:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,22 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('message', message=> {
-    if (message.author.bot) return;
-    if (message.isMentioned(client.user))
-    {
-    message.reply(" كيف اقدر اساعدك!!");
-    }
-});
+client.on('message', message => {
+         if (message.content.startsWith(prefix + "#p")) {
+         if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+                 if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
+          message.guild.createChannel('☄-★-welcome-☆-☇ۨ', 'text')
+          message.guild.createChannel('☄-★-rules-☆-☇', 'text')
+          message.guild.createChannel('☄-★chat-☆-☇', 'text')
+          message.guild.createChannel('ּ☄-★-youtube-☆-☇', 'text')
+          message.guild.createChannel('ּ☄-★-giveaway-☆-☇', 'text') 
+          message.guild.createChannel('ּ☄-★-bot-spam-☆-☇', 'text')
+          message.guild.createChannel('ۭCreate a server', 'text')
 
+
+
+
+
+     message.channel.sendMessage('**Channel Was Succsesfluy Created**')
+     }
+     });
 client.login(process.env.BOT_TOKEN);
